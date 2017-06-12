4e2244a6deef4440fb3d173bce9603222a5f0fc2
sdelamo <sergio.delamo@softamo.com>
2017-06-12 12:36:45 +0200

    Add missing expression

--- complete/client/src/config.js
+++ complete/client/src/config.js
@@ -1,5 +1,10 @@
 import pjson from './../package.json';
 
-export const SERVER_URL = 'http://localhost:8080/';
+const prod = process.env.NODE_ENV === 'production';  // <1>
+
+console.log(`Loading ${process.env.NODE_ENV} config...`);
+
+export const SERVER_URL = prod ? '' : 'http://localhost:8080';
+
 export const CLIENT_VERSION = pjson.version;
 export const REACT_VERSION = pjson.dependencies.react;
